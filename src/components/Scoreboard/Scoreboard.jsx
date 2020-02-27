b67c45b5db1e909cd538d50fcdc48784ea159f6b
girbola <marko.lokka@gmail.com>
2020-02-27 19:20:30 +0200

    First commit

---
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+const Scoreboard = props => {
+
+};
+
+export default Scoreboard;
