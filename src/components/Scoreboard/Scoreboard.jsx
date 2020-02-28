aee5edbb7dcaae66f79e94b47b77597176437173
girbola <marko.lokka@gmail.com>
2020-02-28 09:19:51 +0200

    Dead code cleaning

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -1,49 +1,99 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import { Form, FormGroup, Button, Container } from 'react-bootstrap';
-import "./Scoreboard.css";
+import { Form, Button, Container } from 'react-bootstrap';
+import './Scoreboard.css';
 
 const Scoreboard = props => {
+	const [idCounter, setIdCounter] = useState(0);
+	const [scoreBoardTableData, setScoreBoardTableData] = useState([]);
+	const [playerName, setPlayerName] = useState('');
+	const [playerPoints, setPlayerPoints] = useState();
 
-	const [station, setStation] = useState([]);
+	useEffect(() => {
+		console.log('Updating! ' + playerName);
+	}, [idCounter]);
 
+	/*
+	* Data columns for Bootstrap Table - react-bootstrap-table2
+	*/
 	const columns = [
 		{
-			dataField: 'name',
-			text: 'Name',
+			dataField: 'id',
+			text: 'ID',
+			hidden: true,
 		},
 		{
-			dataField: 'score',
-			text: 'Score',
+			dataField: 'playerName',
+			text: 'Name',
+			sort: true,
+		},
+		{
+			dataField: 'playerPoints',
+			text: 'Points',
 			sort: true,
 		},
 	];
-	function addToTable(str) {}
-	const data = [];
+	const defaultSorted = [
+		{
+			dataField: 'playerPoints',
+			order: 'desc',
+		},
+	];
+	/*
+	 *	Add name and points to table
+	 */
+	function addToTable() {
+		console.log('UPDATING addToTable');
+		if (playerName.length >= 1 && playerPoints >= 1) {
+			setIdCounter(idCounter + 1);
+			const list = { id: idCounter, playerName: playerName, playerPoints: playerPoints };
+			scoreBoardTableData.push(list);
+		}
+	}
+
 	return (
-        
-        <Container>
-		<div className="ScoreBoard">
-			<div className={'TextBox'}>
-				<BootstrapTable keyField="scoreboard" data={data} columns={columns} striped hoves></BootstrapTable>
+		<Container fluid>
+			<div className="ScoreBoard">
+				<div className={'Table'}>
+					<BootstrapTable
+						keyField="id"
+						isKey={true}
+						data={scoreBoardTableData}
+						columns={columns}
+						striped
+						hoves
+						defaultSorted={defaultSorted}
+					></BootstrapTable>
+				</div>
 
-				<Form className="Form">
-					<Form.Group controlId="Score">
-						{/* <Form.Label>Score</Form.Label> */}
-						<Form.Control type="text" placeholder="Enter name" />
-
-						<Form.Control type="text" placeholder="Enter score" />
-						<Button variant="primary" className="Button">
-							Add
-						</Button>
-					</Form.Group>
-				</Form>
+				
+				<div className="FormStyleContainer">
+					<Form className="Form">
+						<Form.Group className="Form" controlId="Points">
+							<Form.Control
+								className="InputPlayerName"
+								type="text"
+								onChange={e => setPlayerName(e.target.value)}
+								placeholder="Enter name"
+							/>
+							<Form.Control
+								className="InputPlayerPoints"
+								type="number"
+								onChange={e => setPlayerPoints(e.target.value)}
+								placeholder="Enter points"
+							/>
+							<Container className="ButtonContainer">
+								<Button onClick={e => addToTable()} className="AddButton" variant="primary">
+									Add
+								</Button>
+							</Container>
+						</Form.Group>
+					</Form>
+				</div>
+				<br />
 			</div>
-			<br />
-            </div>
-        </Container>
-        
+		</Container>
 	);
 };
 
