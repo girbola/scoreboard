33302a59fd3114ab538db8e19cfc3212e886a658
girbola <marko.lokka@gmail.com>
2020-02-28 12:26:39 +0200

    Fixed form functionality

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import { Form, Button, Container } from 'react-bootstrap';
@@ -7,6 +7,7 @@ import './Scoreboard.css';
 const Scoreboard = props => {
 	const [idCounter, setIdCounter] = useState(0);
 	const [scoreBoardTableData, setScoreBoardTableData] = useState([]);
+	
 	const [playerName, setPlayerName] = useState('');
 	const [playerPoints, setPlayerPoints] = useState();
 
@@ -15,8 +16,8 @@ const Scoreboard = props => {
 	}, [idCounter]);
 
 	/*
-	* Data columns for Bootstrap Table - react-bootstrap-table2
-	*/
+	 * Data columns for Bootstrap Table - react-bootstrap-table2
+	 */
 	const columns = [
 		{
 			dataField: 'id',
@@ -40,21 +41,42 @@ const Scoreboard = props => {
 			order: 'desc',
 		},
 	];
+
 	/*
-	 *	Add name and points to table
+	 * Adding new name with points into scoreBoardTableData list {id: int, playerNamer: string, playerPoints: int}
+	 */
+	function addNewName() {
+		setIdCounter(idCounter + 1);
+		// const player = playerName.trim();
+		console.log('Player name:' + playerName + ':');
+		const list = { id: idCounter, playerName: playerName, playerPoints: playerPoints };
+		scoreBoardTableData.push(list);
+	}
+
+	/*
+	 *	Add name and points to table using scoreBoardTableData array
 	 */
 	function addToTable() {
-		console.log('UPDATING addToTable');
-		if (playerName.length >= 1 && playerPoints >= 1) {
-			setIdCounter(idCounter + 1);
-			const list = { id: idCounter, playerName: playerName, playerPoints: playerPoints };
-			scoreBoardTableData.push(list);
+		const points = parseInt(playerPoints);
+		console.log('UPDATING addToTable: ' + playerName + ' points: ' + points);
+		if (playerName.length >= 1 && points >= 1) {
+			addNewName();
 		}
+	}
+
+	/*
+	 * Validating points from string to integer
+	 */
+	function validateNumber(e) {
+		console.log('Validating number' + typeof e.target.value);
+		const number = parseInt(e.target.value);
+		setPlayerPoints(number);
 	}
 
 	return (
 		<Container fluid>
 			<div className="ScoreBoard">
+				<h2>Scoreboard</h2>
 				<div className={'Table'}>
 					<BootstrapTable
 						keyField="id"
@@ -67,31 +89,27 @@ const Scoreboard = props => {
 					></BootstrapTable>
 				</div>
 
-				
-				<div className="FormStyleContainer">
-					<Form className="Form">
-						<Form.Group className="Form" controlId="Points">
-							<Form.Control
-								className="InputPlayerName"
-								type="text"
-								onChange={e => setPlayerName(e.target.value)}
-								placeholder="Enter name"
-							/>
-							<Form.Control
-								className="InputPlayerPoints"
-								type="number"
-								onChange={e => setPlayerPoints(e.target.value)}
-								placeholder="Enter points"
-							/>
-							<Container className="ButtonContainer">
-								<Button onClick={e => addToTable()} className="AddButton" variant="primary">
-									Add
-								</Button>
-							</Container>
-						</Form.Group>
-					</Form>
-				</div>
-				<br />
+				<Form className="FormContainer">
+					<Form.Group className="Form" controlId="Points">
+						<Form.Control
+							className="InputPlayerName"
+							type="text"
+							onChange={e => setPlayerName(e.target.value)}
+							placeholder="Enter name"
+						/>
+						<Form.Control
+							className="InputPlayerPoints"
+							type="number"
+							onChange={e => validateNumber(e)}
+							placeholder="Enter points"
+						/>
+						<Container className="ButtonContainer">
+							<Button onClick={e => addToTable()} className="AddButton" variant="primary">
+								Add
+							</Button>
+						</Container>
+					</Form.Group>
+				</Form>
 			</div>
 		</Container>
 	);
